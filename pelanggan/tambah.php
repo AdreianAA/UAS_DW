c003b36dbcd37e2077a617bfc4a67582310326fd
Luckyftr <luckyfitrianda1@gmail.com>
2026-06-20 09:21:42 +0700

    Tukar File aja

--- pelanggan/tambah.php
+++ pelanggan/tambah.php
@@ -4,55 +4,41 @@ include '../config/koneksi.php';
 
 if(isset($_POST['simpan'])){
 
-$id_produk = $_POST['id_produk'];
-$id_pelanggan = $_POST['id_pelanggan'];
-$id_waktu = $_POST['id_waktu'];
-$jumlah = $_POST['jumlah'];
+    $kode_pelanggan = $_POST['kode_pelanggan'];
+    $nama_pelanggan = $_POST['nama_pelanggan'];
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $kota = $_POST['kota'];
 
-$getHarga = mysqli_fetch_assoc(
-mysqli_query(
-$conn,
-"SELECT harga
-FROM dim_produk
-WHERE id_produk='$id_produk'"
-));
+    mysqli_query(
+        $conn,
+        "INSERT INTO dim_pelanggan
+        (
+            kode_pelanggan,
+            nama_pelanggan,
+            jenis_kelamin,
+            kota
+        )
+        VALUES
+        (
+            '$kode_pelanggan',
+            '$nama_pelanggan',
+            '$jenis_kelamin',
+            '$kota'
+        )"
+    );
 
-$harga_satuan = $getHarga['harga'];
-
-$total_harga = $jumlah * $harga_satuan;
-
-mysqli_query(
-$conn,
-"INSERT INTO fact_penjualan
-(
-id_produk,
-id_pelanggan,
-id_waktu,
-jumlah,
-harga_satuan,
-total_harga
-)
-VALUES
-(
-'$id_produk',
-'$id_pelanggan',
-'$id_waktu',
-'$jumlah',
-'$harga_satuan',
-'$total_harga'
-)"
-);
-
-header("Location:index.php");
+    header("Location:index.php");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Tambah Transaksi</title>
+
+<title>Tambah Pelanggan</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
 
@@ -60,27 +46,37 @@ header("Location:index.php");
 
 <div class="container mt-4">
 
-<h3>Tambah Transaksi</h3>
+<h3>Tambah Pelanggan</h3>
 
 <form method="POST">
 
 <div class="mb-3">
+<label>Kode Pelanggan</label>
 
-<label>Produk</label>
+<input type="text"
+       name="kode_pelanggan"
+       class="form-control"
+       required>
+</div>
 
-<select name="id_produk" class="form-control">
+<div class="mb-3">
+<label>Nama Pelanggan</label>
 
-<?php
-$produk = mysqli_query($conn,"SELECT * FROM dim_produk");
+<input type="text"
+       name="nama_pelanggan"
+       class="form-control"
+       required>
+</div>
 
-while($p=mysqli_fetch_assoc($produk)){
-?>
+<div class="mb-3">
 
-<option value="<?= $p['id_produk']; ?>">
-<?= $p['nama_produk']; ?>
-</option>
+<label>Jenis Kelamin</label>
 
-<?php } ?>
+<select name="jenis_kelamin"
+        class="form-control">
+
+<option value="L">Laki-Laki</option>
+<option value="P">Perempuan</option>
 
 </select>
 
@@ -88,70 +84,25 @@ while($p=mysqli_fetch_assoc($produk)){
 
 <div class="mb-3">
 
-<label>Pelanggan</label>
+<label>Kota</label>
 
-<select name="id_pelanggan" class="form-control">
-
-<?php
-$pelanggan = mysqli_query($conn,"SELECT * FROM dim_pelanggan");
-
-while($pl=mysqli_fetch_assoc($pelanggan)){
-?>
-
-<option value="<?= $pl['id_pelanggan']; ?>">
-<?= $pl['nama_pelanggan']; ?>
-</option>
-
-<?php } ?>
-
-</select>
-
-</div>
-
-<div class="mb-3">
-
-<label>Tanggal</label>
-
-<select name="id_waktu" class="form-control">
-
-<?php
-$waktu = mysqli_query($conn,"SELECT * FROM dim_waktu");
-
-while($w=mysqli_fetch_assoc($waktu)){
-?>
-
-<option value="<?= $w['id_waktu']; ?>">
-<?= $w['tanggal']; ?>
-</option>
-
-<?php } ?>
-
-</select>
-
-</div>
-
-<div class="mb-3">
-
-<label>Jumlah</label>
-
-<input
-type="number"
-name="jumlah"
-class="form-control"
-required>
+<input type="text"
+       name="kota"
+       class="form-control"
+       required>
 
 </div>
 
 <button type="submit"
-name="simpan"
-class="btn btn-success">
+        name="simpan"
+        class="btn btn-success">
 
 Simpan
 
 </button>
 
 <a href="index.php"
-class="btn btn-secondary">
+   class="btn btn-secondary">
 
 Kembali
 
--- transaksi/tambah.php
+++ transaksi/tambah.php
@@ -4,41 +4,55 @@ include '../config/koneksi.php';
 
 if(isset($_POST['simpan'])){
 
-    $kode_pelanggan = $_POST['kode_pelanggan'];
-    $nama_pelanggan = $_POST['nama_pelanggan'];
-    $jenis_kelamin = $_POST['jenis_kelamin'];
-    $kota = $_POST['kota'];
+$id_produk = $_POST['id_produk'];
+$id_pelanggan = $_POST['id_pelanggan'];
+$id_waktu = $_POST['id_waktu'];
+$jumlah = $_POST['jumlah'];
 
-    mysqli_query(
-        $conn,
-        "INSERT INTO dim_pelanggan
-        (
-            kode_pelanggan,
-            nama_pelanggan,
-            jenis_kelamin,
-            kota
-        )
-        VALUES
-        (
-            '$kode_pelanggan',
-            '$nama_pelanggan',
-            '$jenis_kelamin',
-            '$kota'
-        )"
-    );
+$getHarga = mysqli_fetch_assoc(
+mysqli_query(
+$conn,
+"SELECT harga
+FROM dim_produk
+WHERE id_produk='$id_produk'"
+));
 
-    header("Location:index.php");
+$harga_satuan = $getHarga['harga'];
+
+$total_harga = $jumlah * $harga_satuan;
+
+mysqli_query(
+$conn,
+"INSERT INTO fact_penjualan
+(
+id_produk,
+id_pelanggan,
+id_waktu,
+jumlah,
+harga_satuan,
+total_harga
+)
+VALUES
+(
+'$id_produk',
+'$id_pelanggan',
+'$id_waktu',
+'$jumlah',
+'$harga_satuan',
+'$total_harga'
+)"
+);
+
+header("Location:index.php");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-
-<title>Tambah Pelanggan</title>
+<title>Tambah Transaksi</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
 </head>
 <body>
 
@@ -46,37 +60,27 @@ if(isset($_POST['simpan'])){
 
 <div class="container mt-4">
 
-<h3>Tambah Pelanggan</h3>
+<h3>Tambah Transaksi</h3>
 
 <form method="POST">
 
 <div class="mb-3">
-<label>Kode Pelanggan</label>
 
-<input type="text"
-       name="kode_pelanggan"
-       class="form-control"
-       required>
-</div>
+<label>Produk</label>
 
-<div class="mb-3">
-<label>Nama Pelanggan</label>
+<select name="id_produk" class="form-control">
 
-<input type="text"
-       name="nama_pelanggan"
-       class="form-control"
-       required>
-</div>
+<?php
+$produk = mysqli_query($conn,"SELECT * FROM dim_produk");
 
-<div class="mb-3">
+while($p=mysqli_fetch_assoc($produk)){
+?>
 
-<label>Jenis Kelamin</label>
+<option value="<?= $p['id_produk']; ?>">
+<?= $p['nama_produk']; ?>
+</option>
 
-<select name="jenis_kelamin"
-        class="form-control">
-
-<option value="L">Laki-Laki</option>
-<option value="P">Perempuan</option>
+<?php } ?>
 
 </select>
 
@@ -84,25 +88,70 @@ if(isset($_POST['simpan'])){
 
 <div class="mb-3">
 
-<label>Kota</label>
+<label>Pelanggan</label>
 
-<input type="text"
-       name="kota"
-       class="form-control"
-       required>
+<select name="id_pelanggan" class="form-control">
+
+<?php
+$pelanggan = mysqli_query($conn,"SELECT * FROM dim_pelanggan");
+
+while($pl=mysqli_fetch_assoc($pelanggan)){
+?>
+
+<option value="<?= $pl['id_pelanggan']; ?>">
+<?= $pl['nama_pelanggan']; ?>
+</option>
+
+<?php } ?>
+
+</select>
+
+</div>
+
+<div class="mb-3">
+
+<label>Tanggal</label>
+
+<select name="id_waktu" class="form-control">
+
+<?php
+$waktu = mysqli_query($conn,"SELECT * FROM dim_waktu");
+
+while($w=mysqli_fetch_assoc($waktu)){
+?>
+
+<option value="<?= $w['id_waktu']; ?>">
+<?= $w['tanggal']; ?>
+</option>
+
+<?php } ?>
+
+</select>
+
+</div>
+
+<div class="mb-3">
+
+<label>Jumlah</label>
+
+<input
+type="number"
+name="jumlah"
+class="form-control"
+required>
 
 </div>
 
 <button type="submit"
-        name="simpan"
-        class="btn btn-success">
+name="simpan"
+class="btn btn-success">
 
 Simpan
 
 </button>
 
 <a href="index.php"
-   class="btn btn-secondary">
+class="btn btn-secondary">
 
 Kembali
 
